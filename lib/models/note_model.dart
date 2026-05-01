0c42cd198b9c02684539d56365399d9b115de6d9
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-05-01 18:51:24 +0530

    Updated the Note model with Subtasks

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,11 +1,30 @@
-class Note{
+class SubTask {
+  final int id;
+  final String title;
+  final bool completed;
 
+  // 1. Normal Constructor
+  SubTask({
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  // 2. Factory Constructor
+  factory SubTask.fromJson(Map<String, dynamic> json) {
+    return SubTask(
+        id: json['id'], title: json['title'], completed: json['completed']);
+  }
+}
+
+class Note {
   final int id;
   final String title;
   final String content;
   final String createdAt;
-  final List subtasks;
+  final List<SubTask> subtasks;
 
+//  1. Normal Constructor
   Note({
     required this.id,
     required this.title,
@@ -14,16 +33,25 @@ class Note{
     required this.subtasks,
   });
 
-  factory Note.fromJSON(Map<String,dynamic> json){
+// 2. factory constructor
+
+  factory Note.fromJson(Map<String, dynamic> json) {
+    // 1. Getting the raw list of map from th json
+    var rawSubtasks = json['subtasks'] as List;
+
+    // 2. Map each "Map" to a "SubTask Object" using the translator
+
+    List<SubTask> subTaskList =
+        rawSubtasks.map((eachMap) => SubTask.fromJson(eachMap)).toList();
+
+    // 3. Return The finished Note object
+
     return Note(
-    id: json['id'],
-    title: json['title'], 
-    content: json['content'], 
-    createdAt: json['created_at'], 
-    subtasks: json['subtask']??[]);
+      id: json['id'],
+      title: json['title'],
+      content: json['content'],
+      createdAt: json['created_at'],
+      subtasks: subTaskList,
+    );
   }
-
- 
-
-
 }
