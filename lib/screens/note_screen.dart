311f9b072076c73f7e0aa05cb33200abf1ca47fc
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-05-02 23:04:20 +0530

    Added Refresh Concept in note App

--- lib/screens/note_screen.dart
+++ lib/screens/note_screen.dart
@@ -26,11 +26,18 @@ class _NoteScreenState extends State<NoteScreen> {
     _fetchNotes(); // Starts our API call immediately
   }
 
-  Future<void> _fetchNotes() async {
-    print("DEBUG: _fetchNotes has started!");
+  Future<void> _fetchNotes({bool isRefresh = false}) async {
+    if (isRefresh) {
+      setState(() {
+        _currentPage = 1;
+        _hasNextPage = true;
+      });
+    }
     // 1. Ui to show the spinner
     setState(() {
-      _isLoading = true;
+      if (!isRefresh) {
+        _isLoading = true;
+      }
     });
 
     // 2. Call the API
@@ -41,8 +48,16 @@ class _NoteScreenState extends State<NoteScreen> {
       // 4. turning the list of  Maps into a list of Note Objects
       final List rawNotes = data['results'];
 
+      //convert json into Note Object
+      List<Note> newNotes =
+          rawNotes.map((json) => Note.fromJson(json)).toList();
+
       setState(() {
-        _notes.addAll(rawNotes.map((json) => Note.fromJson(json)).toList());
+        if (isRefresh) {
+          _notes = newNotes;
+        } else {
+          _notes.addAll(newNotes);
+        }
 
         //checking next page exists in django
         _hasNextPage = data['next'] != null;
@@ -65,64 +80,67 @@ class _NoteScreenState extends State<NoteScreen> {
       ),
       body: _isLoading
           ? Center(child: CircularProgressIndicator())
-          : ListView.builder(
-              itemCount: _hasNextPage ? _notes.length + 1 : _notes.length,
-              itemBuilder: (context, index) {
-                if (index == _notes.length) {
-                  return Padding(
-                    padding: EdgeInsetsGeometry.all(16),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        setState(
-                          () {
-                            _currentPage++;
-                          },
-                        );
-                        _fetchNotes();
-                      },
-                      child: Text("Load More"),
+          : RefreshIndicator(
+              onRefresh: () => _fetchNotes(isRefresh: true),
+              child: ListView.builder(
+                itemCount: _hasNextPage ? _notes.length + 1 : _notes.length,
+                itemBuilder: (context, index) {
+                  if (index == _notes.length) {
+                    return Padding(
+                      padding: EdgeInsetsGeometry.all(16),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(
+                            () {
+                              _currentPage++;
+                            },
+                          );
+                          _fetchNotes();
+                        },
+                        child: Text("Load More"),
+                      ),
+                    );
+                  }
+
+                  final note = _notes[index];
+                  return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: ListTile(
+                      title: Text(
+                        note.title,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        note.content,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      trailing: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            // Ternary Operator
+                            note.subtasks.isNotEmpty &&
+                                    note.subtasks.every((s) => s.completed)
+                                ? Icons.check_circle // If Yes Filled icon
+                                : Icons
+                                    .radio_button_unchecked, // if No Outline icon
+
+                            // Color condition
+                            color: note.subtasks.isNotEmpty &&
+                                    note.subtasks.every((s) => s.completed)
+                                ? Colors.green
+                                : Colors.grey,
+                            size: 18,
+                          ),
+                          Text(
+                              "${note.subtasks.where((s) => s.completed).length}/${note.subtasks.length}"),
+                        ],
+                      ),
                     ),
                   );
-                }
-
-                final note = _notes[index];
-                return Card(
-                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  child: ListTile(
-                    title: Text(
-                      note.title,
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                      note.content,
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    trailing: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          // Ternary Operator
-                          note.subtasks.isNotEmpty &&
-                                  note.subtasks.every((s) => s.completed)
-                              ? Icons.check_circle // If Yes Filled icon
-                              : Icons
-                                  .radio_button_unchecked, // if No Outline icon
-
-                          // Color condition
-                          color: note.subtasks.isNotEmpty &&
-                                  note.subtasks.every((s) => s.completed)
-                              ? Colors.green
-                              : Colors.grey,
-                          size: 18,
-                        ),
-                        Text(
-                            "${note.subtasks.where((s) => s.completed).length}/${note.subtasks.length}"),
-                      ],
-                    ),
-                  ),
-                );
-              },
+                },
+              ),
             ),
     );
   }
