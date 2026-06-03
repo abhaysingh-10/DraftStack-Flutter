3daeb943be5ca5a2ae8d13c7848b4c71568e3c68
Abhay Singh <aiwithabhay7474452@gmail.com>
2026-06-03 20:08:29 +0530

    Type 4 : Notifier Provider class Created

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,4 +1,41 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes_app/services/api_services.dart';
+
+// 1. The Notifier Provider
+
+class AuthNotifier extends Notifier<AuthState>{
+
+  @override
+  AuthState build(){
+    return AuthState.initial();
+  }
+
+ //login action
+
+ Future<void> login(String username,String password) async{
+  state = AuthState.loading();
+
+  final apiService = ref.watch(apiServiceProvider);
+  final success = await apiService.login(username, password);
+
+  if(success){
+    state = AuthState.authenticated();
+  }else{
+    state = AuthState.error("Invalid Username and Password");
+  }
+ }
+
+// logout  action
+
+Future<void> logout() async{
+  await ref.read(apiServiceProvider).logout();
+  state = AuthState.unauthenticated();
+}
+}
+
+
+
+
 
 enum AuthStatus { initial, loading, authenticated, unauthenticated, error }
 
